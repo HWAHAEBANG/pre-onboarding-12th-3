7389b9e04459eec585192799f7b9ad6492729fb2
HHB <bcl0206@gmail.com>
2023-09-06 15:50:11 +0900

    style: formatting 적용

--- src/apis/suggestion.ts
+++ src/apis/suggestion.ts
@@ -1,8 +1,8 @@
 import { instance } from "../configs/axios";
 
 export const getSuggestionApi = (searchKeyword: string) => {
-  if(searchKeyword === '') return Promise.resolve({data:[]});
-  console.info("calling api")
+  if (searchKeyword === "") return Promise.resolve({ data: [] });
+  console.info("calling api");
   return instance({
     url: `/sick?q=${searchKeyword}`,
     method: "get",
