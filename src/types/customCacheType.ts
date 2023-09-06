3ed39ef420bb59c0740a82405d010bab1ded2cb8
HHB <bcl0206@gmail.com>
2023-09-06 19:37:45 +0900

    feat: 캐시 관련 타입 모듈화

---
+++ src/types/customCacheType.ts
@@ -0,0 +1,12 @@
+import { Suggestions } from "./searchType";
+
+export interface Cache {
+  [key: string]: { value: Suggestions; expireTime: number };
+}
+
+export interface CacheContextValue {
+  set: (key: string, value: Suggestions, ttl?: number) => void;
+  get: (key: string) => Suggestions | null;
+  remove: (key: string) => void;
+  clear: () => void;
+}
