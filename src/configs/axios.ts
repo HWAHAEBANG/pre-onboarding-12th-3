616c1156a914977b6f13d000a8a6059d96841ab9
HHB <bcl0206@gmail.com>
2023-09-07 09:43:38 +0900

    chore: 불필요 헤더 삭제

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -6,12 +6,9 @@ export const instance = axios.create({
   headers: {
     "Access-Control-Allow-Credentials": "true",
     "Cache-Control": "no-cache",
-    // Connection: 'keep-alive',//
-    // 'Content-Length': '401',//
     "Content-Type": "application/json; charset=utf-8",
     ETag: 'W/"191-7DnIoxk/oktj4DgKFpQhLQmCm7M"',
     Expires: "-1",
-    // 'Keep-Alive': 'timeout=5',//
     Pragma: "no-cache",
     Vary: "Origin, Accept-Encoding",
     "X-Content-Type-Options": "nosniff",
